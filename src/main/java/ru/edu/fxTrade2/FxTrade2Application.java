e9dc5b98f7e9512046f31d27d3f7dbd814d6fc94
Nikolay Rogozin <koliatoa@gmail.com>
2024-11-27 20:45:24 +0300

    Inirial commit

---
+++ src/main/java/ru/edu/fxTrade2/FxTrade2Application.java
@@ -0,0 +1,13 @@
+package ru.edu.fxTrade2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxTrade2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxTrade2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/edu/fxTrade2/FxTrade2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.edu.fxTrade2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxTrade2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
